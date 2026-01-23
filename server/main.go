0dc17cb87d3e528bf6aa52fbf7811844e71c4b3f
Travis Harmon <harmtrav@gmail.com>
2026-01-23 08:24:36 -0600

    Refactor server initialization to use http.Server for improved configuration

--- server/main.go
+++ server/main.go
@@ -4,12 +4,23 @@ import (
 	"main/handlers"
 	"main/log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	srv := createServer()
+	panic(srv.ListenAndServe())
+}
+
+func createServer() *http.Server {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", log.Middleware(handlers.Assets))
 
-	panic(http.ListenAndServe(":10003", mux))
+	return &http.Server{
+		Addr:         ":10003",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 60,
+	}
 }
