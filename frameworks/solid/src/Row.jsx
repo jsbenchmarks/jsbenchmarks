ca3f5913e1b5eb199fbbbd167f756e32595b0ecf
Travis Harmon <harmtrav@gmail.com>
2026-01-23 12:15:20 -0600

    Use textContent prop for Solid implementation

--- frameworks/solid/src/Row.jsx
+++ frameworks/solid/src/Row.jsx
@@ -16,35 +16,45 @@ function Row({ row }) {
       class={isSelected(row.id) ? 'selected' : ''}
       onClick={() => setSelected(row.id)}
     >
-      <td>{row.id}</td>
-      <td>{row.name}</td>
-      <td>
-        {(row.weight * weightConversion()).toFixed(1)}{' '}
-        {unitmap.weight[unitSystem()]}
-      </td>
-      <td>
-        {(row.dimensions.height * lengthConversion()).toFixed(1)} x{' '}
-        {(row.dimensions.width * lengthConversion()).toFixed(1)} x{' '}
-        {(row.dimensions.depth * lengthConversion()).toFixed(1)}{' '}
-        {unitmap.length[unitSystem()]}
-      </td>
-      <td>
-        {(row.powerConsumption * powerConversion()).toFixed(1)}{' '}
-        {unitmap.power[unitSystem()]}
-      </td>
-      <td>${row.price[0]().toFixed(2)}</td>
-      <td>{row.availabilityStatus[0]()}</td>
-      <td>{row.rating.toFixed(1)}</td>
+      <td textContent={row.id} />
+      <td textContent={row.name} />
+      <td
+        textContent={
+          (row.weight * weightConversion()).toFixed(1) +
+          ' ' +
+          unitmap.weight[unitSystem()]
+        }
+      />
+      <td
+        textContent={
+          (row.dimensions.height * lengthConversion()).toFixed(1) +
+          ' x ' +
+          (row.dimensions.width * lengthConversion()).toFixed(1) +
+          ' x ' +
+          (row.dimensions.depth * lengthConversion()).toFixed(1) +
+          ' ' +
+          unitmap.length[unitSystem()]
+        }
+      />
+      <td
+        textContent={
+          (row.powerConsumption * powerConversion()).toFixed(1) +
+          ' ' +
+          unitmap.power[unitSystem()]
+        }
+      />
+      <td textContent={'$' + row.price[0]().toFixed(2)} />
+      <td textContent={row.availabilityStatus[0]()} />
+      <td textContent={row.rating.toFixed(1)} />
       <td>
         <button
           class="small"
+          textContent="delete"
           onClick={(e) => {
             e.stopPropagation();
             setRows(rows().filter((r) => r.id !== row.id));
           }}
-        >
-          delete
-        </button>
+        />
       </td>
     </tr>
   );
