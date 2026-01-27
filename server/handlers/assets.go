71e3a3ce18cb3e083aa1a70983d403e8318f1115
Travis Harmon <harmtrav@gmail.com>
2026-01-27 16:05:12 -0600

    Fix asset serving for react router

--- server/handlers/assets.go
+++ server/handlers/assets.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"os"
 	"path"
 	"path/filepath"
 	"strings"
@@ -12,11 +13,17 @@ func Assets(w http.ResponseWriter, r *http.Request) {
 	if name == "" {
 		name = "index.html"
 	}
-	switch filepath.Ext(name) {
+	fullPath := path.Join("assets", name)
+	if _, er := os.Stat(fullPath); os.IsNotExist(er) {
+		fullPath = path.Join("assets", "index.html")
+	}
+	switch filepath.Ext(fullPath) {
 	case ".js", ".css":
 		w.Header().Set("Cache-Control", "public, max-age=31536000, immutable")
 	case ".png", ".svg":
 		w.Header().Set("Cache-Control", "public, max-age=3600")
+	case ".json":
+		w.Header().Set("Cache-Control", "public, max-age=60")
 	}
-	http.ServeFile(w, r, path.Join("assets", name))
+	http.ServeFile(w, r, fullPath)
 }
