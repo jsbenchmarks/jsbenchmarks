54ec5a539d64a3993a6876d8e255ec847e5a46d0
Travis Harmon <harmtrav@gmail.com>
2026-02-04 11:01:58 -0600

    Add thyn

--- frameworks/thyn/src/state.thyn.js
+++ frameworks/thyn/src/state.thyn.js
@@ -1,7 +1,16 @@
+import { unitmap } from "common/data.js";
+
+const { weight, length, power } = unitmap;
+
 export const data = $signal([]);
 export const selected = $signal(null);
 export const unitSystem = $signal("metric");
+export const stopStreaming = $signal(null);
 
 export const weightConversion = () => unitSystem() === "metric" ? 1 : 2.20462;
 export const powerConversion = () => unitSystem() === "metric" ? 1 : 0.00134102;
 export const lengthConversion = () => unitSystem() === "metric" ? 1 : 0.393701;
+
+export const weightUnits = () => weight[unitSystem()];
+export const lengthUnits = () => length[unitSystem()];
+export const powerUnits = () => power[unitSystem()];
